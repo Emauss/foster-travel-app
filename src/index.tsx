04693134227a8b6b11b712ac4ce6591c548aec77
Emanuel Okrój <emanuel.okroj@flow2code.com>
2023-01-12 13:57:08 +0100

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/global.scss";
+
+ReactDOM.render(<h1>Hello React!</h1>, document.getElementById("app-root"));
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const prod = process.env.NODE_ENV === "production";
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: prod ? "production" : "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/public/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".ts", ".tsx", ".js", ".json"],
+        },
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: ["./public"],
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
+  devtool: prod ? undefined : "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
